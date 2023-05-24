c5ebd627cb6e8dbad2eb570e770d6b67bef6713b
Freddy Mendez misena <freddy.mendez@misena.edu.co>
2023-05-24 11:25:32 -0500

    GUI

---
+++ Ejercicio_Vendedor/src/com/example/main/Inicio.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+import com.example.gui.VentanaMenu;
+
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaMenu ventana = new VentanaMenu();
+        ventana.setVisible(true);
+    }
+    
+}
